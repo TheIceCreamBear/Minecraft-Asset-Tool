40671f617043d5fa3af741dfe024c46a98cbc627
Joseph <kartingman25@gmail.com>
2019-06-19 13:27:57 -0400

    Create basic Index Parser

---
+++ src/com/joseph/mat/json/MinecraftAsset.java
@@ -0,0 +1,25 @@
+package com.joseph.mat.json;
+
+public class MinecraftAsset {
+	private String key;
+	private String hash;
+	private long size;
+	
+	public MinecraftAsset(String key, String hash, long size) {
+		this.key = key;
+		this.hash = hash;
+		this.size = size;
+	}
+	
+	public String getHash() {
+		return this.hash;
+	}
+	
+	public String getKey() {
+		return this.key;
+	}
+	
+	public long getSize() {
+		return this.size;
+	}
+}
---
+++ src/com/joseph/mat/json/MinecraftIndexParser.java
@@ -0,0 +1,39 @@
+package com.joseph.mat.json;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.MalformedJsonException;
+
+public class MinecraftIndexParser {
+	public static HashMap<String, MinecraftAsset> parse(File file) throws IOException {
+		JsonReader reader = new JsonReader(new FileReader(file));
+		HashMap<String, MinecraftAsset> map = new HashMap<String, MinecraftAsset>();
+		reader.beginObject();
+		if (!reader.nextName().equals("objects")) {
+			reader.close();
+			throw new MalformedJsonException("Invalid index file");
+		}
+		reader.beginObject();
+		while (reader.peek() != JsonToken.END_OBJECT && reader.peek() != JsonToken.END_DOCUMENT) {
+			String name = reader.nextName();
+			reader.beginObject();
+			reader.nextName(); // hash
+			String hash = reader.nextString();
+			reader.nextName(); // size
+			long size = reader.nextLong();
+			MinecraftAsset ma = new MinecraftAsset(name, hash, size);
+			map.put(name, ma);
+			reader.endObject();
+		}
+		reader.endObject();
+		reader.endObject();
+		reader.close();
+		// DOCUMENT END
+		return map;
+	}
+}
