a1d75a0f6b8b2b9a7d5b666c037fa2d8703cadba
Joseph <kartingman25@gmail.com>
2019-06-18 22:30:10 -0400

    Add reference class

---
+++ src/com/joseph/mat/reference/Reference.java
@@ -0,0 +1,8 @@
+package com.joseph.mat.reference;
+
+public class Reference {
+	public static final String MINECRAFT_ROOT_DIR = System.getProperty("user.home") + "/.minecraft";
+	public static final String MINECRAFT_ASSETS_DIR = MINECRAFT_ROOT_DIR + "/assets";
+	public static final String MINECRAFT_ASSETS_INDEX_DIR = MINECRAFT_ASSETS_DIR + "/indexes";
+	public static final String MINECRAFT_ASSETS_OBJECTS = MINECRAFT_ASSETS_DIR + "/objects";
+}
