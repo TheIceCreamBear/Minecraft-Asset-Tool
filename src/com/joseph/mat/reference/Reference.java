a152e094f2b66f15094809d2e20673b0c3abdf79
Joseph <kartingman25@gmail.com>
2020-10-18 14:51:03 -0400

    Make reference use File.separator instead of hardcoded /

--- src/com/joseph/mat/reference/Reference.java
+++ src/com/joseph/mat/reference/Reference.java
@@ -1,8 +1,10 @@
 package com.joseph.mat.reference;
 
+import java.io.File;
+
 public class Reference {
-	public static final String MINECRAFT_ROOT_DIR = System.getProperty("user.home") + "/.minecraft";
-	public static final String MINECRAFT_ASSETS_DIR = MINECRAFT_ROOT_DIR + "/assets";
-	public static final String MINECRAFT_ASSETS_INDEX_DIR = MINECRAFT_ASSETS_DIR + "/indexes";
-	public static final String MINECRAFT_ASSETS_OBJECTS = MINECRAFT_ASSETS_DIR + "/objects";
+	public static final String MINECRAFT_ROOT_DIR = System.getProperty("user.home") + File.separator + ".minecraft";
+	public static final String MINECRAFT_ASSETS_DIR = MINECRAFT_ROOT_DIR + File.separator + "assets";
+	public static final String MINECRAFT_ASSETS_INDEX_DIR = MINECRAFT_ASSETS_DIR + File.separator + "indexes";
+	public static final String MINECRAFT_ASSETS_OBJECTS = MINECRAFT_ASSETS_DIR + File.separator + "objects";
 }
