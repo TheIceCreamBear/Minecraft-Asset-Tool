682741a6143f92baa28bda1b114de06d4221b3bb
Joseph <kartingman25@gmail.com>
2020-10-19 03:56:24 -0400

    Finalize main features of the program

--- src/com/joseph/mat/Main.java
+++ src/com/joseph/mat/Main.java
@@ -1,8 +1,12 @@
 package com.joseph.mat;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.channels.FileChannel;
 import java.util.Arrays;
 import java.util.HashMap;
 
@@ -21,12 +25,13 @@ public class Main {
 	public static void main(String[] args) {
 		if (System.getProperty("os.name").toLowerCase().contains("mac")) {
 			JOptionPane.showMessageDialog(null, "This program does not currently support macOS");
+			// TODO make sure this works on macOS (OSX) and Linux distros
 		}
-		if ((System.getProperty("os.name").contains("Windows") || System.getProperty("os.name").contains("windows")) && !System.getProperty("user.home").contains("AppData")) {
+		if ((System.getProperty("os.name").contains("Windows") || System.getProperty("os.name").contains("windows"))) {
 			System.setProperty("user.home", System.getProperty("user.home") + File.separator + "AppData" + File.separator + "Roaming");
 		}
 		try {
-			// Make the LaF of Swing the Windows LaF
+			// Make the LaF of Swing the System LaF
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
 			System.err.println("Unable to properly set Look and Feel.");
@@ -68,14 +73,90 @@ public class Main {
 	
 	public static void printAssetMap(HashMap<String, MinecraftAsset> map, boolean useTestString) {
 		if (useTestString) {
+			MinecraftAsset max = new MinecraftAsset("notmax", "invalid", -1);
 			for (String name : map.keySet()) {
+				if (map.get(name).getSize() > max.getSize()) {
+					max = map.get(name);
+				}
 				System.out.println(name + ": " + map.get(name).toTestString());
 			}
+			System.out.println(max.getSize());
 			return;
 		}
 		
 		for (String name : map.keySet()) {
 			System.out.println(name + ": " + map.get(name).toString());
 		}
+	}
+	
+	public static void convertSelectedMinecraftAssets(MinecraftAsset[] assets, String destinationDir) {
+		LoadingDialog ld = new LoadingDialog(assets.length);
+		
+		for (int i = 0; i < assets.length; i++) {
+			// update status
+			ld.updateProgressBar(i, "Processing file " + assets[i].toString() + ": " + i + "/" + assets.length);
+			MinecraftAsset current = assets[i];
+			
+			// calculate the source file
+			String objectFolder = current.getHash().substring(0, 2);
+			String fileSource = Reference.MINECRAFT_ASSETS_OBJECTS + File.separator + objectFolder + File.separator + current.getHash();
+			
+			// calculate the destination file
+			String currentKeyFixed = current.getKey().replace('/', File.separatorChar);
+			String fileDestinationParent = destinationDir + File.separator;
+			if (currentKeyFixed.contains(File.separator)) {
+				 fileDestinationParent = fileDestinationParent + currentKeyFixed.substring(0, currentKeyFixed.lastIndexOf(File.separatorChar));
+			}
+			String fileDestination = destinationDir + File.separator + currentKeyFixed;
+//			System.out.println(fileSource + ": " + fileDestination);
+			
+			// open the files needed
+			File source = new File(fileSource);
+			File destinationParent = new File(fileDestinationParent);
+			File destination = new File(fileDestination);
+			
+			// if the source file doesn't exist (for some reason) go to the next loop
+			if (!source.exists()) {
+				continue;
+			}
+			
+			// make sure that the folders exist
+			if (!destinationParent.exists()) {
+				destinationParent.mkdirs();
+			}
+			
+			// make sure that the file exists
+			if (!destination.exists()) {
+				try {
+					destination.createNewFile();
+				} catch (IOException e) {
+					e.printStackTrace();
+					continue;
+				}
+			}
+			
+			// code resources found while looking for (better) ways to copy a file 
+			// from one location to another
+			// https://stackoverflow.com/questions/2520305/java-io-to-copy-one-file-to-another
+			// https://stackoverflow.com/questions/106770/standard-concise-way-to-copy-a-file-in-java
+			
+			// try with resources with many resources looks gross
+			// try with resources code convention (maybe?)
+			// https://stackoverflow.com/questions/30753996/what-is-the-code-convention-for-formatting-try-with-resources
+			try (FileInputStream inStream = new FileInputStream(source);
+				 FileOutputStream outStream = new FileOutputStream(destination);
+				 FileChannel src = inStream.getChannel();
+				 FileChannel dest = outStream.getChannel())
+			{
+				// transfer the contents of the soruce file to the destination file
+				dest.transferFrom(src, 0, current.getSize());
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		ld.closeFrame();
 	}
 }
