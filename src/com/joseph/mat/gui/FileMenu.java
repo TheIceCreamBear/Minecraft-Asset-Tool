14c24f26ea6a250cf29ec7b3f8ec020eca5db82c
Joseph <kartingman25@gmail.com>
2020-10-29 01:36:52 -0400

    Add basic FileMenu class

---
+++ src/com/joseph/mat/gui/FileMenu.java
@@ -0,0 +1,28 @@
+package com.joseph.mat.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class FileMenu extends JMenu {
+	private static final long serialVersionUID = 7439222685484561479L;
+	
+	private JMenuItem chooseAssetsDirectory;
+	
+	// TODO does this need more
+
+	public FileMenu() {
+		super("File");
+		
+		this.chooseAssetsDirectory = new JMenuItem("Choose new Asset Directory");
+		this.chooseAssetsDirectory.setToolTipText("Opens a dialog to select a new directory to search");
+		this.chooseAssetsDirectory.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO fill this in
+			}
+		});
+	}
+}
