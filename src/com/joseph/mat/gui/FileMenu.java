2b3f6dff4340479608f9d887a0916bfe5f588af6
Joseph <kartingman25@gmail.com>
2020-11-03 00:09:27 -0500

    Start to add FileMenu functionality

--- src/com/joseph/mat/gui/FileMenu.java
+++ src/com/joseph/mat/gui/FileMenu.java
@@ -3,13 +3,17 @@ package com.joseph.mat.gui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFileChooser;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+
+import com.joseph.mat.reference.Reference;
 
 public class FileMenu extends JMenu {
 	private static final long serialVersionUID = 7439222685484561479L;
 	
 	private JMenuItem chooseAssetsDirectory;
+	private JMenuItem resetAssetsDirectory;
 	
 	// TODO does this need more
 
@@ -20,10 +24,27 @@ public class FileMenu extends JMenu {
 		this.chooseAssetsDirectory.setToolTipText("Opens a dialog to select a new directory to search");
 		this.chooseAssetsDirectory.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// TODO fill this in
+				JFileChooser chooser = new JFileChooser();
+				chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				int result = chooser.showOpenDialog(null);
+				if (result == JFileChooser.APPROVE_OPTION) {
+					String file = chooser.getSelectedFile().getAbsolutePath();
+					Reference.updateRootDir(file);
+					// TODO make GUI main reset
+				}
+			}
+		});
+		
+		this.resetAssetsDirectory = new JMenuItem("Reset Assets Directory");
+		this.resetAssetsDirectory.setToolTipText("Resets the assets directory to the default state");
+		this.resetAssetsDirectory.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Reference.resetRootDir();
+				// TODO make GUI main reset
 			}
 		});
 		
 		this.add(chooseAssetsDirectory);
+		this.add(resetAssetsDirectory);
 	}
 }
